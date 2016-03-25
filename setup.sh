4f4e595453e2ffe5a648e5dd7a32917f3cc28b02
ryoe <ryoeshi@yahoo.co.jp>
2016-03-25 18:07:38 +0900

    zsh向けの設定を調整

--- setup.sh
+++ setup.sh
@@ -3,27 +3,40 @@
 # check command.
 git --version >/dev/null
 
-dot_dir=dotfiles
-
-cd ${HOME}
-pwd
-
 echo 'create symbolic links.'
+dot_dir=${HOME}/dotfiles
 for n in ${dot_dir}/dot.*
 do
-  if [ ! -e $n ] ; then
-    ln -s $n `echo $n | sed -e "s/${dot_dir}\/dot././"`
+  filename=`echo $n | sed -e "s:${dot_dir}/dot.:.:"`
+  dest_path=${HOME}/${filename}
+  if [ ! -e ${dest_path} ] ; then
+    ln -s $n ${dest_path}
   fi
 done
 
-#pm_dir=~/.vim/dein/repos/dein.vim
-#pm_repo=https://github.com/Shougo/dein.vim.git
-pm_dir=~/.vim/bundle/neobundle.vim
-pm_repo=https://github.com/Shougo/neobundle.vim
-
 echo 'get plugin manager.'
-mkdir -p ${pm_dir}
-git clone ${pm_repo} ${pm_dir}
+#pm_dir=${HOME}/.vim/dein/repos/dein.vim
+#pm_repo=https://github.com/Shougo/dein.vim.git
+pm_dir=${HOME}/.vim/bundle/neobundle.vim
+pm_repo=https://github.com/Shougo/neobundle.vim
+if [ ! -d ${pm_dir} ] ; then
+  mkdir -p ${pm_dir}
+  git clone ${pm_repo} ${pm_dir}
+fi
+
+zprezto_dir=${HOME}/.zprezto/runcoms
+if [ -d "${zprezto_dir}" ] ; then
+  echo 'custom zsh config.'
+  custom_dir=${dot_dir}/custom/zprezto-runcoms
+  for n in ${custom_dir}/*
+  do
+    filename=`echo $n | sed -e "s:${custom_dir}/::"`
+    dest_path=${zprezto_dir}/${filename}
+    if [ ! -e "${dest_path}" ] ; then
+      ln -s $n ${dest_path}
+    fi
+  done
+fi
 
 echo 'setup completed.'
 
